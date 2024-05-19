8576b1da92c58d5160d14edec6005979e90fb053
Leon de França <151105361+leonstro@users.noreply.github.com>
2024-05-19 02:02:11 -0300

    Add files via upload

---
+++ Projeto/script.js
@@ -0,0 +1,29 @@
+var header = document.getElementById('header');
+var navegar2 = document.getElementById('navegar2');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar()
+{
+  showSidebar = !showSidebar
+  if(showSidebar)
+  {
+    navegar2.style.marginLeft = '-10vw';
+    navegar2.style.animationName = 'showSidebar';
+    content.style.filter = 'blur(2px)'
+
+  }
+  else
+  {
+    navegar2.style.marginLeft = '-100vw';
+    navegar2.style.animationName = '';
+    content.style.filter = ''
+  }
+}
+function closeSidebar()
+{
+  if(showSidebar)
+  {
+    toggleSidebar();
+  }
+}
